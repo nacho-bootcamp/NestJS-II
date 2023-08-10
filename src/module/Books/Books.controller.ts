a168b311988f12ca1dc2049f56a0332a82037ab5
ignacio leonel cardozo <ignaciocardozoxd12@gmail.com>
2023-08-09 23:42:37 -0300

    dto

--- src/module/Books/Books.controller.ts
+++ src/module/Books/Books.controller.ts
@@ -1,12 +1,23 @@
-import { Controller, Get } from "@nestjs/common";
+import { Body, Controller, Get, Post } from "@nestjs/common";
 import { BooksService } from "./Books.service";
+import { BooksEntity } from "./entities/Books.entity";
+import { CreateBookDto } from "./dtos/books.dto";
 
 @Controller("books") // va la ruta de navegacion
 export class BooksControllers {
   constructor(private bookservice: BooksService) { }
   @Get()
-  todos() {
-    const books = this.bookservice.findAllBooks()
+  //una funcion que va a devolver una promesa que se resolvera como un array de booksentity
+  //dentro tenemos una variable de tipo array entity que es igual a this.bookservice.findAllBooks()
+  async findAll(): Promise<BooksEntity[]> {
+    const books: BooksEntity[] = await this.bookservice.findAllBooks()
     return books
+  }
+
+  @Post()
+
+  async createBook(@Body() newBook: CreateBookDto): Promise<BooksEntity> {
+    const newbook: BooksEntity = await this.bookservice.createBooks(newBook)
+    return newbook
   }
 }
--- src/module/Books/Books.service.ts
+++ src/module/Books/Books.service.ts
@@ -1,5 +1,6 @@
 import { Injectable } from "@nestjs/common";
 import { BooksEntity } from "./entities/Books.entity";
+import { CreateBookDto } from './dtos/books.dto';
 
 @Injectable()
 export class BooksService {
@@ -27,6 +28,16 @@ export class BooksService {
   async findAllBooks(): Promise<BooksEntity[]> { // esta funcion va a devolver una promesa que se resulve a un
     //array de libros
     return this.stock
+  }
+
+  async createBooks(createBookDto: CreateBookDto): Promise<BooksEntity> {
+    const newBooks: BooksEntity = {
+      id: this.stock.length + 1,
+      nombre: createBookDto.nombre,
+      autor: createBookDto.autor,
+    }
+    this.stock.push(newBooks)
+    return newBooks
   }
 }
 
